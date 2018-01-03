9d372c65dd84bc8b112c1c5e94c90eebf3c85c08
chenkai <1151337324@qq.com>
2018-01-03 15:40:30 +0800

    no message

--- test.php
+++ test.php
@@ -5,14 +5,15 @@ require 'vendor/autoload.php';
 use Leonis\Digiccy\Wallet;
 
 $config = [
-    'symbol'          => 'ETH',
-    'ip'              => '49.4.64.171',
-    'port'            => 8545,
+    'symbol'          => 'SMC',
+    'ip'              => '127.0.0.1',
+    'port'            => 8299,
     'user'            => 'user',
     'password'        => 'password',
-    'wallet_password' => '123456',
-    'erc'             => 'gec22',
+    'type'            => '3'
+//    'wallet_password' => '123456',
+//    'erc'             => 'gec22',
 ];
 
 $btc = new Wallet($config);
-print_r($btc->sendToAddress(['0x93ae19169bc6f43f8806aac6248e56319c10a8e6','0x3d3b296b93a6ec7d78fcfd76a06f2b1a67fcd8af','2.1']));
+print_r($btc->getWalletBalance());
